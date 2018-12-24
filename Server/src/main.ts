a62df56c26428df6f136cb9af46e0e9f0396fb18
Romain Duvillier <rdu@dfakto.com>
2018-12-24 16:06:04 +0100

    remove body-parser

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -5,27 +5,7 @@ import { Config } from './Client/config';
 export const app = express();
 const cors = require('cors');
 const config = new Config();
-const corsOptions = {
-  origin: "https://rizdelhuile.rduvi.com"
-};
-
-//app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json({limit:'50mb'}));
-
-app.use(cors(corsOptions));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.post('/', function(req, res, next) {
- // Handle the post for this route
-});
-
-app.get('/info', function (req, res) {
-    res.send('La purée c\'est cool!')
-  });
+app.use(cors());
 
 //#region Routes
 userRoutes(app);
