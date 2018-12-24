91813085fc05732320411e153482afa09024d4ed
Romain Duvillier <rdu@dfakto.com>
2018-12-24 16:15:55 +0100

    update corsOptions

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -5,7 +5,16 @@ import { Config } from './Client/config';
 export const app = express();
 const cors = require('cors');
 const config = new Config();
-app.use(cors());
+var corsOptions = {
+  origin: 'https://rizdelhuile.rduvi.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+app.get('*', cors(corsOptions), function (req, res, next) {
+  next();
+})
 
 //#region Routes
 userRoutes(app);
